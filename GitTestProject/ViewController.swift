e7bf8c546a573ba920b8f5ab3d62d4b17a41724e
ihcet <ihcet>
2021-05-03 21:52:42 +0300

    Initial Commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by i. halil çetinkaya on 3.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
